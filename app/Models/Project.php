b8f7d8ecaad6052f8ee27c91a2d34023c76f4941
antonionovelli92 <anto.novelli@hotmail.it>
2023-03-15 16:43:34 +0100

    first commit

---
+++ app/Models/Project.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title', 'author', 'description', 'content', 'slug', 'image', 'url_project', 'url_generic', 'is_published', 'category_id'];
+
+
+    // Assegno la relazione con le categorie
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    // Assegno la relazione con i tag
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+}
